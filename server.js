104cf7e68ff6101c980485837d3dae90e2584c1c
Eve <evelyn.sanchez.2104.2019@gmail.com>
2025-04-22 16:53:00 -0300

    update server js

--- server.js
+++ server.js
@@ -4,8 +4,35 @@ const path = require('path');
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
 
+const server = http.createServer((req, res) => {
+  let filePath = '';
+  
+  switch (req.url) {
+    case '/index':
+      filePath = path.join(__dirname, 'public', 'index.html');
+      break;
+    case '/about':
+      filePath = path.join(__dirname, 'public', 'about.html');
+      break;
+    case '/contact':
+      filePath = path.join(__dirname, 'public', 'contact.html');
+      break;
+    default:
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.end('<h1>Error 404: Pagina no encontrada</h1>');
+      return;
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/html' });
+      res.end('<h1>Error 500: Error interno del servidor</h1>');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
+    }
+  });
 });
 
 server.listen(PORT, () => {
